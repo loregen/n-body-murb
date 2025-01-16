9fd18331c7ae47a04414d42fbdb96698fd91044b
Lorenzo Gentile <lorenzo.gentile2001@hotmail.it>
2025-01-16 17:39:14 +0100

    add debug for cuda.

--- src/murb/implem/SimulationNBodyCuda.cu
+++ src/murb/implem/SimulationNBodyCuda.cu
@@ -8,7 +8,19 @@
 #include "SimulationNBodyCuda.cuh"
 
 #define MAX_SHARED_PER_BLOCK 48000
-#define THREADS_PER_BLK 1024
+#define THREADS_PER_BLK 512
+
+#define CUDA_CHECK(call)                                                     \
+    do                                                                       \
+    {                                                                        \
+        cudaError_t error = call;                                            \
+        if (error != cudaSuccess)                                            \
+        {                                                                    \
+            fprintf(stderr, "CUDA error at %s:%d: %s\n", __FILE__, __LINE__, \
+                    cudaGetErrorString(error));                              \
+            exit(EXIT_FAILURE);                                              \
+        }                                                                    \
+    } while (0)
 
 namespace cuda
 {
@@ -25,18 +37,28 @@ namespace cuda
         cudaGetDeviceProperties(&prop, i);
         printf("Device Number: %d\n", i);
         printf("  Device name: %s\n", prop.name);
-        printf("  Memory Clock Rate (MHz): %d\n",
-               prop.memoryClockRate / 1024);
-        printf("  Memory Bus Width (bits): %d\n",
-               prop.memoryBusWidth);
-        printf("  Peak Memory Bandwidth (GB/s): %.1f\n",
+        printf("  Compute capability: %d.%d\n", prop.major, prop.minor);
+        printf("  Total global memory (Gbytes): %.1f\n", (float)(prop.totalGlobalMem) / 1024.0 / 1024.0 / 1024.0);
+        printf("  Shared memory per block (Kbytes): %.1f\n", (float)(prop.sharedMemPerBlock) / 1024.0);
+        printf("  Total shared memory per multiprocessor (Kbytes): %.1f\n", (float)(prop.sharedMemPerMultiprocessor) / 1024.0);
+        printf("  Registers per block: %d\n", prop.regsPerBlock);
+        printf("  Warp size: %d\n", prop.warpSize);
+        printf("  Max threads per block: %d\n", prop.maxThreadsPerBlock);
+        printf("  Max threads per multiprocessor: %d\n", prop.maxThreadsPerMultiProcessor);
+        printf("  Max blocks per dimension: %d x %d x %d\n",
+               prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+        printf("  Max threads per dimension: %d x %d x %d\n",
+               prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+        printf("  Number of multiprocessors: %d\n", prop.multiProcessorCount);
+        printf("  Memory clock rate (MHz): %d\n", prop.memoryClockRate / 1024);
+        printf("  Memory bus width (bits): %d\n", prop.memoryBusWidth);
+        printf("  Peak memory bandwidth (GB/s): %.1f\n",
                2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
-        printf("  Total global memory (Gbytes) %.1f\n", (float)(prop.totalGlobalMem) / 1024.0 / 1024.0 / 1024.0);
-        printf("  Shared memory per block (Kbytes) %.1f\n", (float)(prop.sharedMemPerBlock) / 1024.0);
-        printf("  minor-major: %d-%d\n", prop.minor, prop.major);
-        printf("  Warp-size: %d\n", prop.warpSize);
         printf("  Concurrent kernels: %s\n", prop.concurrentKernels ? "yes" : "no");
-        printf("  Concurrent computation/communication: %s\n\n", prop.deviceOverlap ? "yes" : "no");
+        printf("  Concurrent computation/communication: %s\n", prop.deviceOverlap ? "yes" : "no");
+        printf("  Clock rate (GHz): %.2f\n", (float)(prop.clockRate) / 1.0e6);
+        printf("  L2 cache size (Kbytes): %.1f\n", (float)(prop.l2CacheSize) / 1024.0);
+        printf("  Compute mode: %d\n", prop.computeMode);
     }
   }
 
@@ -84,20 +106,20 @@ namespace cuda
     {
       //load the last tile
       shared_mem[threadIdx.x] = global_AoS[tileIdx];
-      __syncthreads();
+    }
+    __syncthreads();
 
-      for(unsigned jBody = 0; jBody < nBodies % THREADS_PER_BLK; jBody++)
-      {
-        float4 otherBody = shared_mem[jBody];
-        float3 rij = {otherBody.x - myBody.x, otherBody.y - myBody.y, otherBody.z - myBody.z};
-        float rijSquared = rij.x * rij.x + rij.y * rij.y + rij.z * rij.z + softSquared;
+    for(unsigned jBody = 0; jBody < nBodies % THREADS_PER_BLK; jBody++)
+    {
+      float4 otherBody = shared_mem[jBody];
+      float3 rij = {otherBody.x - myBody.x, otherBody.y - myBody.y, otherBody.z - myBody.z};
+      float rijSquared = rij.x * rij.x + rij.y * rij.y + rij.z * rij.z + softSquared;
 
-        float ai = G * otherBody.w / (rijSquared * sqrtf(rijSquared));
+      float ai = G * otherBody.w / (rijSquared * sqrtf(rijSquared));
 
-        acc.x += ai * rij.x;
-        acc.y += ai * rij.y;
-        acc.z += ai * rij.z;
-      }
+      acc.x += ai * rij.x;
+      acc.y += ai * rij.y;
+      acc.z += ai * rij.z;
     }
 
     if(iBody < nBodies)
@@ -118,7 +140,15 @@ SimulationNBodyCuda::SimulationNBodyCuda(const unsigned long nBodies, const std:
     this->accelerations.resize(this->getBodies().getN());
 
     //print CUDA device properties of the current device
-    //cuda::printGPUInfo();
+    cuda::printGPUInfo();
+
+    cudaFuncAttributes attr;
+    cudaFuncGetAttributes(&attr, cuda::computeBodiesAccell_k);
+    std::cout << "Shared memory per block required by the kernel: " << attr.sharedSizeBytes << " bytes" << std::endl;
+
+    int minGridSize, blockSize;
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, cuda::computeBodiesAccell_k, 0, 0);
+    std::cout << "Max potential block size: " << blockSize << " threads" << std::endl;
 
 }
 
@@ -159,23 +189,27 @@ void SimulationNBodyCuda::computeBodiesAcceleration()
     void *d_acc;
 
     // allocate memory on the device
-    cudaMalloc(&d_AoS, 4 * n * sizeof(float));
-    cudaMalloc(&d_acc, 3 * n * sizeof(float));
+    CUDA_CHECK(cudaMalloc(&d_AoS, 4 * n * sizeof(float)));
+    CUDA_CHECK(cudaMalloc(&d_acc, 3 * n * sizeof(float)));
 
     //copy body data on device
     // cudaMemcpy(d_AoS, d.data(), 4 * n * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_AoS, d_new.data(), 4 * n * sizeof(float), cudaMemcpyHostToDevice);
+    CUDA_CHECK(cudaMemcpy(d_AoS, d_new.data(), 4 * n * sizeof(float), cudaMemcpyHostToDevice));
 
     int numBlocks = (n + THREADS_PER_BLK - 1) / THREADS_PER_BLK;
 
+    std::cout << "Launching kernel with " << numBlocks << " blocks and " << THREADS_PER_BLK << " threads per block" << std::endl;
     cuda::computeBodiesAccell_k<<<numBlocks, THREADS_PER_BLK>>>(d_AoS, d_acc, n, this->soft * this->soft, this->G);
 
+    //check for errors
+    CUDA_CHECK(cudaPeekAtLastError());
+
     //copy back the result
-    cudaMemcpy(this->accelerations.data(), d_acc, 3 * n * sizeof(float), cudaMemcpyDeviceToHost);
+    CUDA_CHECK(cudaMemcpy(this->accelerations.data(), d_acc, 3 * n * sizeof(float), cudaMemcpyDeviceToHost));
 
     //free memory
-    cudaFree(d_AoS);
-    cudaFree(d_acc);
+    CUDA_CHECK(cudaFree(d_AoS));
+    CUDA_CHECK(cudaFree(d_acc));
 }
 
 void SimulationNBodyCuda::computeOneIteration()
